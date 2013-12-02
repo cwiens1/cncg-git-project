76aab2cd1ee46aa796d4ced8b7cd25eb2fcaf0fc
Christoph Wiens <christoph.wiens@gmail.com>
2013-12-02 12:54:07 +0000

    Added parameter to App

--- my-java-project/src/main/java/com/mycompany/app/App.java
+++ my-java-project/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,34 @@
 package com.mycompany.app;
 
 /**
- * Hello world!
+ * My Subversion Test App
  *
  */
 public class App 
 {
+	private String name;
+	
+	public App (String name) {
+		this.name = name;
+	}
+	
+	public String sayMyName() {
+		return "My name is: " + getName();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello Subversion!" );
+		App app = new App("My Subversion Test App");
+		
+        System.out.println( app.sayMyName() );
     }
 }
